1360ee2e7e0007ba0e23f8fd485135b4ffd55b0a
Vitor Cruz <vitor.dasilvacruz@gmail.com>
2020-02-17 14:44:21 -0300

    homepage

--- src/Mock/mock.js
+++ src/Mock/mock.js
@@ -7,25 +7,25 @@ export default [
         id: 1,
         content: 'Study React Redux',
         labels: ['#7159c1'],
-        user: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png'
+        user: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Happy&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light'
       },
       {
         id: 2,
         content: 'Create youtube content',
         labels: ['#7159c1'],
-        user: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png'
+        user: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairBun&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=Gray02&eyeType=Default&eyebrowType=RaisedExcitedNatural&mouthType=Default&skinColor=Brown'
       },
       {
         id: 3,
         content: 'Build React Native App',
         labels: ['#7159c1'],
-        user: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png'
+        user: 'https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Sunglasses&facialHairType=BeardLight&facialHairColor=Platinum&clotheType=GraphicShirt&clotheColor=Black&graphicType=Bat&eyeType=Close&eyebrowType=Default&mouthType=Default&skinColor=DarkBrown'
       },
       {
         id: 4,
         content: 'Improve english',
         labels: ['#54e1f7'],
-        user: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png'
+        user: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairBun&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=Gray02&eyeType=Default&eyebrowType=RaisedExcitedNatural&mouthType=Default&skinColor=Brown'
       },
     ]
   },
@@ -37,13 +37,13 @@ export default [
         id: 5,
         content: 'Deploy Portfolio',
         labels: ['#54e1f7'],
-        user: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png'
+        user: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Happy&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light'
       },
       {
         id: 6,
         content: 'Recreate Telegram clone',
         labels: [],
-        user: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png'
+        user: 'https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Sunglasses&facialHairType=BeardLight&facialHairColor=Platinum&clotheType=GraphicShirt&clotheColor=Black&graphicType=Bat&eyeType=Close&eyebrowType=Default&mouthType=Default&skinColor=DarkBrown'
       }
     ]
   },
@@ -55,13 +55,13 @@ export default [
         id: 7,
         content: 'Take some vacations',
         labels: ['#7159c1'],
-        user: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png'
+        user: 'https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Sunglasses&facialHairType=BeardLight&facialHairColor=Platinum&clotheType=GraphicShirt&clotheColor=Black&graphicType=Bat&eyeType=Close&eyebrowType=Default&mouthType=Default&skinColor=DarkBrown'
       },
       {
         id: 8,
         content: 'Buy a Car',
         labels: ['#54e1f7'],
-        user: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png'
+        user: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Happy&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light'
       },
       {
         id: 9,
